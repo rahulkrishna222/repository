cc98c39c877d595a025cb8ddbeac4a12fe86c57d
Rahul K <rahulkrishna222@gmail.com>
2013-08-17 15:44:57 +0530

    + Added some util functions prototypes only.

--- Grandmaster.com/src/com/grandmaster/db/connection/DBConnector.java
+++ Grandmaster.com/src/com/grandmaster/db/connection/DBConnector.java
@@ -2,4 +2,22 @@ package com.grandmaster.db.connection;
 
 public class DBConnector {
 
+    private static boolean loadProperties() {
+        return false;
+    }
+
+    public void createConnection() {
+
+    }
+    
+    public boolean closeConnection(){
+        return false;
+    }
+    
+    public void getConnection(){
+        
+    }
+    
+    
+
 }
---
+++ Grandmaster.com/src/com/grandmaster/util/GrandmasterUtil.java
@@ -0,0 +1,19 @@
+package com.grandmaster.util;
+
+import java.sql.Timestamp;
+
+public class GrandmasterUtil {
+
+    public static String getMD5Hash(String source) {
+        return null;
+    }
+
+    public static String convertTimestampToString(Timestamp source) {
+        return null;
+    }
+
+    public static Timestamp convertStringToTimestamp(String source) {
+        return null;
+    }
+
+}
