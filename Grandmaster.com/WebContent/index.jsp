c751573fb0aec8dffe3647e0b70df8925f42a953
Rahul K <rahulkrishna222@gmail.com>
2013-08-25 14:43:25 +0530

    + Added logging to the project. + Almost completed DBConnector (Testing required). + Web.xml cleanup.

--- Grandmaster.com/WebContent/index.jsp
+++ Grandmaster.com/WebContent/index.jsp
@@ -1,12 +1,37 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
+	pageEncoding="ISO-8859-1"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<style type="text/css">
+h3 {
+	color: #2E85DB;
+}
+
+html,body {
+	margin: 0;
+	padding: 0;
+}
+
+.body_bg {
+	background-color: #ADDBD7;
+}
+
+.div_main {
+	width: 800px;
+	text-align: center;
+	top: 40%;
+	left: 23%;
+	margin: 0 auto;
+	position: absolute;
+}
+</style>
 <title>Grandmaster.com</title>
 </head>
-<body>
-
+<body class="body_bg">
+	<div class="div_main">
+		<h3>Welcome to Grandmaster.com</h3>
+	</div>
 </body>
 </html>
--- Grandmaster.com/src/com/grandmaster/db/connection/DBConnector.java
+++ Grandmaster.com/src/com/grandmaster/db/connection/DBConnector.java
@@ -1,54 +1,94 @@
 package com.grandmaster.db.connection;
 
+import java.io.IOException;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 import java.sql.Connection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 public class DBConnector {
-    private static final String DB_DRIVER_CLASS = "com.mysql.jdbc.Driver";
+
+    private final static Log    logger               = LogFactory.getLog(DBConnector.class);
+
+    private final static String DB_PROPERTY_FILENAME = "grandmaster_db.properties";
+
+    private static final String DB_DRIVER_CLASS      = "com.mysql.jdbc.Driver";
+    private static Properties   grandMasterProps     = null;
 
     public Connection           con;
 
     static {
         try {
             // Registering mysql driver
+            logger.info("Loading mysql driver to classloader : " + DB_DRIVER_CLASS);
             Class.forName(DB_DRIVER_CLASS);
+            logger.info("Completed Loading mysql driver to classloader : " + DB_DRIVER_CLASS);
         } catch (ClassNotFoundException e) {
-            // TODO Add logger for error displaying
-            e.printStackTrace();
+            logger.error("Exception while loading mysql driver to classloader : " + DB_DRIVER_CLASS, e);
         }
-        loadProperties();//
+        loadProperties();// Load DB properties
     }
 
     private static boolean loadProperties() {
-        // Write code to load property from file
-        return false;
+        logger.info("Loading DB properties from file");
+        grandMasterProps = new Properties();
+        try {
+            grandMasterProps.load(DBConnector.class.getResourceAsStream(DB_PROPERTY_FILENAME));
+            logger.info("DB properties loaded successfully");
+        } catch (IOException ioe) {
+            logger.error("IOException while loading database properties, filename : " + DB_PROPERTY_FILENAME, ioe);
+            grandMasterProps.putAll(getDefaultDBConfig());
+        }
+        System.setProperties(grandMasterProps);
+        return true;
+    }
+
+    private static Map<String, String> getDefaultDBConfig() {
+        Map<String, String> properties = new HashMap<String, String>();
+
+        properties.put("grandmaster.db.host", "localhost");
+        properties.put("grandmaster.db.port", "3303");
+        properties.put("grandmaster.db.name", "grandmaster");
+        properties.put("grandmaster.db.user", "root");
+        properties.put("grandmaster.db.password", "pa$$word");
+
+        return properties;
     }
 
     public void createConnection() {
+        logger.debug("Creating new DB connection");
         try {
-            con = DriverManager.getConnection("Need to create DB URL");
+            con = DriverManager.getConnection(getDB_URL(), grandMasterProps.getProperty("grandmaster.db.user"),
+                    grandMasterProps.getProperty("grandmaster.db.password"));
+            logger.debug("Creating new DB connection");
         } catch (SQLException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+            logger.fatal("Failed to create DB Connection , System exiting ", e);
+            System.exit(1);
         }
 
     }
 
     public boolean closeConnection() {
+        logger.debug("Closing DB connection...");
         if (con != null) {
             try {
                 con.close();
+                logger.debug("DB connection closed successfully");
                 return true;
             } catch (SQLException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
+                logger.error("Failed to close DB connection", e);
                 return false;
             }
+        } else {
+            logger.info("There is no connections created");
+            return true;
         }
-
-        return false;
     }
 
     public Connection getConnection() {
@@ -57,6 +97,13 @@ public class DBConnector {
 
     public boolean isConnected() {
         return (con != null);
+    }
+
+    private static String getDB_URL() {
+        String URL = "jdbc://";
+        URL += grandMasterProps.getProperty("grandmaster.db.host") + ":" + grandMasterProps.getProperty("grandmaster.db.port");
+        URL += "/" + grandMasterProps.getProperty("grandmaster.db.name");
+        return URL;
     }
 
 }
